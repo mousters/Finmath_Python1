9519a6f54392a6577dad4ac25d8bdb98e8515af4
mousters <lolmousters@users.noreply.github.com>
2019-11-24 14:11:47 -0600

    no

---
+++ Python Lecture Code/Lecture2/time_decorator.py
@@ -0,0 +1,25 @@
+import functools
+import time
+def timer(func):
+    @functools.wraps(func)
+    def wrapper_timer(*args):
+        #The special syntax *args in function definitions in python is used to pass a variable number of arguments to a function
+        #double star allows us to pass through keyword arguments; used to pass a keyworded, variable-length argument list
+        start_time=time.perf_counter()
+        value=func(*args)
+        end_time=time.perf_counter()
+        run_time=end_time-start_time
+        print(f"Finished {func.__name__!r} in { run_time:.4f} secs")
+        return value
+    return wrapper_timer
+
+@timer
+def counting(num):
+    temp=0
+    for i in range(num):
+        temp+=i
+    return temp
+
+if __name__ == '__main__':
+    counting(1)
+    counting(10000000)
